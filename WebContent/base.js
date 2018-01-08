d75cb069252049fff54ad593f621738cb89cc56f
Adam Holmes <adam.holmes@bcbssc.com>
2018-01-08 08:20:18 -0500

    initial

---
+++ WebContent/base.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/* JSON Example
+$(document).ready(function(){
+    $("#getOpsview").click(function(){
+		 console.log("click Opsview");
+        $.getJSON("https://a70lqcomopvw002.bcbssc.com/rest", function(result){
+            $.each(result, function(key, value){ 
+				   var markup = "<tr><td>" + key + "</td><td>" + value + "</td></tr>";
+				   $("table tbody").append(markup);
+				});
+        });
+    });
+});
+*/

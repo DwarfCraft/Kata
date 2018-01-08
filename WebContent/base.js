100b976448ef6bf778687014d24622f102a98e34
Adam Holmes <adam.holmes@bcbssc.com>
2018-01-08 08:50:32 -0500

    some updates

--- WebContent/base.js
+++ WebContent/base.js
@@ -14,3 +14,30 @@ $(document).ready(function(){
     });
 });
 */
+
+var testJSON = '{"open": "8:00","close": "5:00","days": "Monday - Thursday"}';
+
+function loadJSON(callback){
+	callback(testJSON);
+	
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', 'data.json', true);
+	xobj.onreadystatechange = function () {
+	if (xobj.readyState == 4 && xobj.status == "200") {
+			// .open will NOT return a value but simply returns undefined in async mode so use a callback
+			callback(xobj.responseText);
+		}
+	}
+	xobj.send(null);
+
+}
+
+// Call to function with anonymous callback
+loadJSON(function(response) {
+	// Do Something with the response e.g.
+	jsonresponse = JSON.parse(response);
+	var hourOpen = jsonresponse.days + ": " + jsonresponse.open + " - " + jsonresponse.close; 
+	document.write("<h2>" + hourOpen + "</h2>");
+
+});

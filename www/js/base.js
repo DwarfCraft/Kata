a763e22c63eaa27abdd16e8c813e06afac04fdff
Adam Holmes <adam.holmes@bcbssc.com>
2018-01-10 14:24:12 -0500

    updated layout

--- www/js/base.js
+++ www/js/base.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
 */
 
 var testJSON = '{"open": "8:00","close": "5:00","days": "Monday - Thursday"}';
+var contactJSON = '{"name": "Adam Holmes","phone": "123-123-1234","email": "adam@adam.com"}';
 //Call to function with anonymous callback
 loadJSON(function(response) {
 	// Do Something with the response e.g.
@@ -26,6 +27,9 @@ loadJSON(function(response) {
 	var hourOpen = jsonresponse.days + ": " + jsonresponse.open + " - " + jsonresponse.close;
 	document.getElementById("hoursText").innerHTML = "<h3>" + hourOpen + "</h3>";
 
+	var contactResponse = JSON.parse(contactJSON);
+	var contactInfo = contactResponse.name + "<br>" + contactResponse.phone + "<br>" + contactResponse.email;
+	document.getElementById("contactText").innerHTML = "<h3>" + contactInfo + "</h3>";
 });
 
 function loadJSON(callback) {
@@ -45,17 +49,19 @@ function loadJSON(callback) {
 }
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.display = "block";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0%";
+    document.getElementById("mySidenav").style.display = "none";
 }
 
 //Listeners
 var closeIcons = document.getElementsByClassName("closebtn");
 for (i = 0; i < closeIcons.length; i++) {
-	console.log(closeIcons[i]);
+	//console.log(closeIcons[i]);
 	if (closeIcons[i].parentElement.id == "alert") {
 		closeIcons[i].addEventListener("click", function() {document.getElementById("alert").style.display='none';});
 	}
@@ -73,13 +79,13 @@ for (i = 0; i < closeIcons.length; i++) {
 var menuOptions = document.getElementsByClassName("menu");
 for (i = 0; i < menuOptions.length; i++) {
 	if (menuOptions[i].id == "menuHours") {
-		menuOptions[i].addEventListener("click", function() {document.getElementById("hours").style.display='block';});
+		menuOptions[i].addEventListener("click", function() {document.getElementById("hours").style.display='block';closeNav();});
 	}
 	if (menuOptions[i].id == "menuContact") {
-		menuOptions[i].addEventListener("click", function() {document.getElementById("contact").style.display='block';});
+		menuOptions[i].addEventListener("click", function() {document.getElementById("contact").style.display='block';closeNav();});
 	}
 	if (menuOptions[i].id == "menuOther") {
-		menuOptions[i].addEventListener("click", function() {document.getElementById("other").style.display='block';});
+		menuOptions[i].addEventListener("click", function() {document.getElementById("other").style.display='block';closeNav();});
 	}
 	if (menuOptions[i].id == "menuHome") {
 		menuOptions[i].addEventListener("click", function() {	
@@ -87,6 +93,7 @@ for (i = 0; i < menuOptions.length; i++) {
 			for (var i = 0; i < menuContent.length; i++) {
 				menuContent[i].style.display = "none";
 			}
+			closeNav();
 		});
 	}	
 	//menuOptions[i].addEventListener("click", menuHandler(menuOptions[i].id));
